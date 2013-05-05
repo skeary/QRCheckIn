ba91531ffcc5fbac61ae54784279a2cc45a78089
Simon Keary <skeary@ImmersiveTechnologies.com>
2013-05-05 21:00:20 +0800

    Improve alert display

--- alert.js
+++ alert.js
@@ -3,7 +3,7 @@
 
 function alertWrapper(message) {
 	if (navigator && navigator.notification) {
-		navigator.notification.alert("message", "Error", "OK"); 
+		navigator.notification.alert(message, "OK", "Error"); 
 	}
 	else {
 		alert(message);
